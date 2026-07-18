b860d711cf180a0ae8bf79371888e24520179338
Jatin <hasijajatin436@gmail.com>
2026-07-18 20:57:44 +0530

    Add Jagged Swaps [A] from Codeforces

---
+++ 1896/A - Jagged Swaps/solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+ 
+    // If the first element is 1, it's possible to sort.
+    if (a[0] == 1) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
