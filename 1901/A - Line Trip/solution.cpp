9747bb9fb89b3b4ab7ba01bb5159f444906ca78f
Jatin <hasijajatin436@gmail.com>
2026-07-18 20:57:27 +0530

    Add Line Trip [A] from Codeforces

---
+++ 1901/A - Line Trip/solution.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        int  k;
+        cin>>n>>k;
+        vector<int> a(n);
+        for(int i=0;i<n;i++) {
+            cin>>a[i];
+        }
+        int maxGap=0;
+        int  prev=0;
+        for(int i=0;i<n;i++) {
+            int current = a[i];
+            int diff = current-prev;
+            maxGap = max(maxGap,diff);
+            prev = current;
+        }
+        // After the loop finishes:
+        int last_gap = 2 * (k - prev);
+        maxGap = max(maxGap, last_gap);
+ 
+        cout << maxGap << endl;
+    }
+    return 0;
+}
