15f3ad4ebdea702abb9dd241dedf5796605ff2de
Jatin <hasijajatin436@gmail.com>
2026-07-18 20:53:53 +0530

    Add Grasshopper on a Line [A] from Codeforces

---
+++ 1837/A - Grasshopper on a Line/solution.cpp
@@ -0,0 +1,30 @@
+        #include <bits/stdc++.h>
+using namespace std;
+ 
+// This function sits outside of main()
+void solve() {
+    int x, k;
+    cin >> x >> k;
+ 
+    if (x % k != 0) {
+        cout << 1 << endl;
+        cout << x << endl;
+    } else {
+        cout << 2 << endl;
+        cout << x - 1 << " " << 1 << endl;
+    }
+}
+ 
+// This is the only entry point of your program
+int main() {
+    // Fast I/O
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
